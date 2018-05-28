c59cb3b9e8259be534b8faae8e6c6e9210487d4d
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 19:11:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,4 @@
-class Dog = {
+class Dog
  def name=(dog_name)
    this_dogs_name=dog_name
  end
@@ -7,7 +7,7 @@ class Dog = {
    this_dogs_name
  end
  
-}
+end
 
 lassie=Dog.new
 lassie.name="Lassie"
